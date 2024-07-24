236f8230062fab826b6bdbfaf9b44846f6685001
ryoheiY <momoyamashia@icloud.com>
2024-07-24 20:40:02 +0900

    リアクティブ方式に移るためにテンプレート駆動の記載を初期化

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,55 +1,9 @@
-import {afterNextRender, Component, DestroyRef, inject, viewChild} from '@angular/core';
-import {FormsModule, NgForm} from "@angular/forms";
-import {debounceTime} from "rxjs";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
-  imports: [FormsModule,]
 })
-export class LoginComponent {
-  private form = viewChild.required<NgForm>('form');
-  private destroyRef = inject(DestroyRef);
-
-  constructor() {
-    afterNextRender(() => {
-      const savedForm = window.localStorage.getItem('saved-login-form');
-
-      if (savedForm){
-        const loadedForm = JSON.parse(savedForm);
-        setTimeout(() => {
-          this.form().controls['email'].setValue(loadedForm.email);
-          this.form().controls['password'].setValue(loadedForm.password);
-        }, 1)
-
-      }
-
-      const subscription = this.form().valueChanges?.pipe(
-        debounceTime(500),
-      ).subscribe({
-        next: (value) => {
-          console.log(value);
-          window.localStorage.setItem('saved-login-form', JSON.stringify(value))
-        },
-      })
-
-      this.destroyRef.onDestroy(() => {subscription?.unsubscribe()})
-    });
-
-  }
-
-  onSubmit(form: NgForm): void {
-    if (form.form.invalid) {
-      console.log("invalid");
-      return;
-    }
-    console.log(form.form);
-    const enteredEmail = form.form.value.email;
-    const password = form.form.value.password;
-    console.log(enteredEmail, password);
-
-    form.form.reset();
-  }
-}
+export class LoginComponent {}
