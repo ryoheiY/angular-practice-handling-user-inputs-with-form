5d7d4fe0f395d9004d8af523b16ad2ac5f541594
ryoheiY <momoyamashia@icloud.com>
2024-07-24 22:17:56 +0900

    reactive方式でFormの再現処理を実装

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,31 @@
-import {Component} from '@angular/core';
-import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {Component, DestroyRef, inject, OnInit} from '@angular/core';
+import {AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {debounceTime, of} from "rxjs";
+
+
+//?マークが含まれていなければエラー
+function mustContainQuestionMark(control : AbstractControl){
+  if(control.value.includes('?')){
+    return null;
+  }
+  return {
+    doesNotContainQuestionMark: true
+  };
+}
+
+function emailIsUnique(control : AbstractControl){
+  if(control.value !== 'test@example.com'){
+    return of(null);
+  }
+  return of({notUnique: true});
+}
+
+let initEmailValue = '';
+const savedForm = window.localStorage.getItem('loginForm');
+if(savedForm) {
+  const loadData = JSON.parse(savedForm);
+  initEmailValue = loadData.email;
+}
 
 @Component({
   selector: 'app-login',
@@ -8,18 +34,22 @@ import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/
   styleUrl: './login.component.css',
   imports: [ReactiveFormsModule]
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+  private destroyRef = inject(DestroyRef);
   form = new FormGroup({
-    email: new FormControl('', {
+    email: new FormControl(initEmailValue, {
       validators: [
         Validators.email,
         Validators.required],
+      asyncValidators: [emailIsUnique]
     }),
     password: new FormControl('', {
       validators: [
         Validators.required,
         Validators.minLength(6),
-        Validators.maxLength(10)],
+        Validators.maxLength(10),
+        mustContainQuestionMark],
+
     }),
   })
 
@@ -36,8 +66,25 @@ export class LoginComponent {
   }
 
   onSubmit() : void {
-    console.log(this.form.value);
+    console.log(this.form);
     console.log(this.form.value.email);
     console.log(this.form.value.password);
+  }
+
+  ngOnInit(): void {
+    // const savedForm = window.localStorage.getItem('loginForm');
+    // if(savedForm){
+    //   const loadData = JSON.parse(savedForm);
+    //   this.form.patchValue({
+    //     email: loadData.email,
+    //   });
+    // }
+
+    const subscription = this.form.valueChanges.pipe(debounceTime(500)).subscribe({
+      next: value => {
+        window.localStorage.setItem("loginForm", JSON.stringify(value));
+      }
+    });
+    this.destroyRef.onDestroy(() => {subscription.unsubscribe();});
   }
 }
