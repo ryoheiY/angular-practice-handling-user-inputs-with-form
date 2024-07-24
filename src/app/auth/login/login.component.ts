162d0231aedee82112aa4e268b8c897204752fdf
ryoheiY <momoyamashia@icloud.com>
2024-07-24 20:53:13 +0900

    reactive方式でFormのバインドを実装

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
   standalone: true,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
+  imports: [ReactiveFormsModule]
 })
-export class LoginComponent {}
+export class LoginComponent {
+  form = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  })
+
+  onSubmit(){
+    console.log(this.form.value);
+  }
+}
